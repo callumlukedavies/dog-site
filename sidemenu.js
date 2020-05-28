122ce16d628e7349fe1f6f42b6837e7ff75cb943
callumlukedavies <callumlukedavies01@gmail.com>
2020-05-28 13:33:06 +0100

    revamp of entire breed page

--- sidemenu.js
+++ sidemenu.js
@@ -1,6 +1,7 @@
 const sidebar = document.getElementById("sidebar");
 const button = document.getElementById("bar");
 const image = document.getElementById("image");
+const box = document.getElementById("box1");
 
 button.addEventListener('click', function(e){
     openMenu();
@@ -23,3 +24,18 @@ function increaseBrightness(){
     image.style.transition = '0.5s';
 }
 
+function flipCard(input, input2){
+    const frontcard = document.getElementById(input);
+    const backcard = document.getElementById(input2);
+    frontcard.style.visibility = 'hidden';
+    backcard.style.opacity = '1';
+    backcard.style.visibility = 'visible';
+}
+
+function reverseCard(input, input2){
+    const frontcard = document.getElementById(input);
+    const backcard = document.getElementById(input2);
+    frontcard.style.visibility = 'visible';
+    backcard.style.visibility = 'hidden';
+    backcard.style.opacity = '0';
+}
